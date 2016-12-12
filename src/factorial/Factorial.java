11e1e35f803aa6501fd7410561907ae4893c052d
damain <damian@danielcastelao.org>
2016-12-12 07:06:52 +0100

    Inicio

---
+++ src/factorial/Factorial.java
@@ -0,0 +1,38 @@
+
+package factorial;
+
+
+public class Factorial {
+
+
+    public static void main(String[] args) {
+
+
+    int j;
+    int f;
+
+  
+   j = 8;    
+   
+
+    
+   int i;
+   if (j == 0)
+f = 1;
+   else
+   {
+f = 1;
+for (i = j; i >= 1; i--)
+{
+    f = f * i;
+}
+   }    
+  
+ 
+
+System.out.println(f); 
+
+
+    }
+    
+}
